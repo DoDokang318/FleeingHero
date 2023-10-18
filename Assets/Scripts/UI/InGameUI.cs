f8603559c4e69bd1105d4d9be9fa99d74377b3b7
Tealss <aitan@naver.com>
2023-10-18 20:05:34 +0900

    save
    save

---
+++ Assets/Scripts/UI/InGameUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+    public GameObject GameUI;
+    private bool isUIVisible = false;
+    private float previousTimeScale;
+
+    private void Start()
+    {
+        previousTimeScale = Time.timeScale;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isUIVisible)
+            {
+                GameUI.SetActive(false);
+                isUIVisible = false;
+                Time.timeScale = previousTimeScale; 
+                Cursor.visible = true;
+            }
+            else
+            {
+                GameUI.SetActive(true);
+                isUIVisible = true;
+                previousTimeScale = Time.timeScale; 
+                Time.timeScale = 0f; 
+                Cursor.visible = true;
+            }
+        }
+    }
+}
