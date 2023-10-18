a1ba2d2bf060b037090745ad15b13da431b3edbb
Tealss <aitan@naver.com>
2023-10-18 10:48:26 +0900

    프롤로그 -
    프롤로그 -

--- Assets/Scripts/UI/Prologue.cs
+++ Assets/Scripts/UI/Prologue.cs
@@ -1,6 +1,8 @@
 using System.Collections;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.ParticleSystem;
 
 public class Prologue : MonoBehaviour
 {
@@ -8,11 +10,12 @@ public class Prologue : MonoBehaviour
     public Text prologueText;
     public AudioManagers audioManagers;
     public CreateCharacter createCharacter;
-
+    public ParticleSystem particle;
     private void Start()
     {
         StartCoroutine(PlayPrologue());
         audioManagers.PitchCheck = true;
+
     }
 
     private void Update()
@@ -25,7 +28,7 @@ public class Prologue : MonoBehaviour
 
     private IEnumerator PlayPrologue()
     {
-        string prologueMessage = "용사 " + CreateCharacter.characterId + "♥여, 어서 일어나게... ";
+        string prologueMessage = "용사 " + CreateCharacter.characterId + "♥님, 일어나세요... ";
 
         for (int i = 0; i < prologueMessage.Length; i++)
         {
@@ -37,9 +40,9 @@ public class Prologue : MonoBehaviour
             prologueText.text += prologueMessage[i];
             if (prologueMessage[i] != ' ' && prologueMessage[i] != ',')
             {
-                
+
                 audioManagers.PlaySound(0);
-           
+
 
             }
 
@@ -48,7 +51,7 @@ public class Prologue : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         prologueText.text = "";
 
-        string prologueMessage2 = "아 뭐야... 잘 자고 있었는데";
+        string prologueMessage2 = "(아 뭐야... 누구야 잘 자고 있었는데)";
 
         for (int i = 0; i < prologueMessage2.Length; i++)
         {
@@ -58,7 +61,7 @@ public class Prologue : MonoBehaviour
             }
 
             prologueText.text += prologueMessage2[i];
-            if (prologueMessage2[i] != ' ' && prologueMessage2[i] != ',')
+            if (prologueMessage2[i] != ' ' && prologueMessage2[i] != ',' && prologueMessage2[i] != '(' && prologueMessage2[i] != ')')
             {
                 audioManagers.PlaySound(0);
             }
@@ -68,7 +71,7 @@ public class Prologue : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         prologueText.text = "";
 
-        string prologueMessage3 = "텍스트 3 테스트용";
+        string prologueMessage3 = CreateCharacter.characterId + "님의 손에, 저희 왕국의 미래가 달려있습니다";
 
         for (int i = 0; i < prologueMessage3.Length; i++)
         {
@@ -79,6 +82,27 @@ public class Prologue : MonoBehaviour
 
             prologueText.text += prologueMessage3[i];
             if (prologueMessage3[i] != ' ' && prologueMessage3[i] != ',')
+            {
+                audioManagers.PlaySound(0);
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        yield return new WaitForSeconds(1.0f);
+        prologueText.text = "";
+        particle.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        string prologueMessage4 = "악의 무리들을 피해, 꼭 저를 구하러 와주세요!!!";
+        prologueText.color = Color.black;
+        for (int i = 0; i < prologueMessage4.Length; i++)
+        {
+            if (skipPrologue)
+            {
+                break;
+            }
+
+            prologueText.text += prologueMessage4[i];
+            if (prologueMessage4[i] != ' ' && prologueMessage4[i] != ',')
             {
                 audioManagers.PlaySound(0);
             }
